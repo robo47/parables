d48595bef3585a292365dc7801abe794bd8597f5
Matthew Lurz <mlurz71@gmail.com>
2009-10-30 03:17:32 -0500

    Updated doctype resource plugin and added a test.

--- Parables/Application/Resource/Doctype.php
+++ Parables/Application/Resource/Doctype.php
@@ -2,21 +2,20 @@
 class Parables_Application_Resource_Doctype extends Zend_Application_Resource_ResourceAbstract
 {
     /**
-     * @var Zend_View
-     */
-    protected $_view = null;
-
-    /**
      * Initialize doctype
      *
      * @return  void
+     * @throws  Zend_Application_Resource_Exception
      */
     public function init()
     {
         $options = $this->getOptions();
-        if (array_key_exists('doctype', $options)) {
-            $doctypeHelper = new Zend_View_Helper_Doctype();
-            $doctypeHelper->doctype($options['doctype']);
+
+        if (!array_key_exists('type', $options)) {
+            throw new Zend_Application_Resource_Exception('Undefined doctype.');
         }
+
+        $doctypeHelper = new Zend_View_Helper_Doctype();
+        $doctypeHelper->doctype($options['type']);
     }
 }
